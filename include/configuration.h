8b1838e3b6d5f69b56e848e45ca5738a230fd86b
ldprice <liam@ldprice.com>
2022-12-20 13:22:35 +1100

    removed-include

--- include/configuration.h
+++
@@ -1,3 +0,0 @@
-/* General configuration file for the Automated Cabinet */
-#define PORTAL_SSID "Automated Cabinet WiFi Connect" // portal SSID
-#define PORTAL_PWD "ACS_1111" // password used to connect to the portal
--- include/mqttConfiguration.h
+++
@@ -1,5 +0,0 @@
-// mqttConfiguration.h
-#define MQTT_SERVER "inside.mqtt.hole"
-#define MQTT_PORT 1883  // Could be anything
-#define MQTT_USERNAME "ACS" // 
-#define MQTT_PASSWORD "ffcxlVZ3jddGw"  // Use a strong password, or don't 
